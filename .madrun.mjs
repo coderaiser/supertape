ef05735a24a0a3d62a001dd3f62b5ffb3bd65975
coderaiser <coderaiser@cloudcmd.io>
2020-12-01 21:15:18 +0200

    feature(supertape) drop support of node < 14

---
+++ .madrun.mjs
@@ -0,0 +1,11 @@
+import {run} from 'madrun';
+
+export default {
+    'test': () => `node bin/supertape.mjs 'test/*.js'`,
+    'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
+    'lint': () => 'putout .',
+    'fix:lint': () => run('lint', '--fix'),
+    'coverage': () => 'nyc npm test',
+    'report': () => 'nyc report --reporter=text-lcov | coveralls',
+};
+
