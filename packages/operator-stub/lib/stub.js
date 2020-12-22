48ab979ecdffcf41e86b7ebb63d29230d9b8d265
coderaiser <coderaiser@cloudcmd.io>
2020-12-22 15:18:50 +0200

    feature(@supertape/operator-stub) add ability to check if provided fn is stub

--- packages/operator-stub/lib/stub.js
+++ packages/operator-stub/lib/stub.js
@@ -1,28 +1,42 @@
+import {isStub} from '@cloudcmd/stub';
 const {stringify} = JSON;
 
-export const called = (operator) => () => {
+const getExpectedStubMessage = (fn) => `Expected stub, but received ${fn.toString()}`;
+
+export const called = (operator) => (fn) => {
+    if (!isStub(fn))
+        return operator.fail(getExpectedStubMessage(fn));
+    
     return operator.fail(`'t.called' is to general, looks like you need 't.calledWith' or 't.calledWithNoArgs'`);
 };
 
 export const notCalled = (operator) => (fn, message = 'should not be called') => {
+    if (!isStub(fn))
+        return operator.fail(getExpectedStubMessage(fn));
+    
     return operator.notOk(fn.called, message);
 };
 
 export const calledWith = (operator) => (fn, args, message = 'should be called with arguments') => {
+    if (!isStub(fn))
+        return operator.fail(getExpectedStubMessage(fn));
+    
     if (!fn.called)
         return operator.fail(`Expected function to be called with arguments '${stringify(args)}', but not called at all`);
     
-    const {length} = fn.args;
-    
-    if (!args || !args.length)
+    if (!args)
         return operator.fail(`You haven't provided 'arguments', looks like you need 't.calledWithNoArgs()'`);
     
+    const {length} = fn.args;
     const calledArgs = fn.args[length - 1];
     
     return operator.deepEqual(calledArgs, args, message);
 };
 
 export const calledWithNoArgs = (operator) => (fn, message = 'should be called with no arguments') => {
+    if (!isStub(fn))
+        return operator.fail(getExpectedStubMessage(fn));
+    
     if (!fn.called)
         return operator.fail('Expected function to be called with no arguments, but not called at all');
     
@@ -33,18 +47,24 @@ export const calledWithNoArgs = (operator) => (fn, message = 'should be called w
 };
 
 export const calledCount = (operator) => (fn, count, message = 'should be called') => {
+    if (!isStub(fn))
+        return operator.fail(getExpectedStubMessage(fn));
+    
     return operator.equal(fn.callCount, count, message);
 };
 
-export const calledOnce = (operator) => (fn, count, message = 'should be called once') => {
-    return operator.equal(fn.callCount, 1, message);
+export const calledOnce = (operator) => (fn, message = 'should be called once') => {
+    return calledCount(operator)(fn, 1, message);
 };
 
 export const calledTwice = (operator) => (fn, count, message = 'should be called twice') => {
-    return operator.equal(fn.callCount, 2, message);
+    return calledCount(operator)(fn, 2, message);
 };
 
 export const calledWithNew = (operator) => (fn, message = 'should be called with new') => {
+    if (!isStub(fn))
+        return operator.fail(getExpectedStubMessage(fn));
+    
     return operator.ok(fn.calledWithNew(), message);
 };
 
--- packages/operator-stub/lib/stub.spec.js
+++ packages/operator-stub/lib/stub.spec.js
@@ -1,5 +1,4 @@
 import {stub, extend} from 'supertape';
-
 import * as operator from './stub.js';
 
 const test = extend(operator);
@@ -8,6 +7,20 @@ test('supertape: operator: stub: not called', (t) => {
     const fn = stub();
     
     t.notCalled(fn);
+    t.end();
+});
+
+test('supertape: operator: stub: not called: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const notCalled = operator.notCalled({
+        fail,
+    });
+    
+    notCalled(fn);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
     t.end();
 });
 
@@ -25,12 +38,40 @@ test('supertape: operator: stub: called', (t) => {
     t.end();
 });
 
+test('supertape: operator: stub: called: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const called = operator.called({
+        fail,
+    });
+    
+    called(fn);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
+    t.end();
+});
+
 test('supertape: operator: stub: called with', (t) => {
     const fn = stub();
     
     fn('hello');
     
     t.calledWith(fn, ['hello']);
+    t.end();
+});
+
+test('supertape: operator: stub: calledWith: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const calledWith = operator.calledWith({
+        fail,
+    });
+    
+    calledWith(fn, []);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
     t.end();
 });
 
@@ -44,12 +85,40 @@ test('supertape: operator: stub: called count', (t) => {
     t.end();
 });
 
+test('supertape: operator: stub: calledCount: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const calledCount = operator.calledCount({
+        fail,
+    });
+    
+    calledCount(fn, 1);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
+    t.end();
+});
+
 test('supertape: operator: stub: called once', (t) => {
     const fn = stub();
     
     fn('hello');
     
     t.calledOnce(fn);
+    t.end();
+});
+
+test('supertape: operator: stub: calledOnce: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const calledOnce = operator.calledOnce({
+        fail,
+    });
+    
+    calledOnce(fn);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
     t.end();
 });
 
@@ -63,7 +132,7 @@ test('supertape: operator: stub: called twice', (t) => {
     t.end();
 });
 
-test('supertape: operator: stub: called twice', (t) => {
+test('supertape: operator: stub: called with new', (t) => {
     const fn = stub();
     
     new fn();
@@ -111,7 +180,7 @@ test('supertape: operator: stub: calledWith: not called', (t) => {
     t.end();
 });
 
-test('supertape: operator: stub: calledWith called with no args', (t) => {
+test('supertape: operator: stub: calledWithNoArgs', (t) => {
     const fn = stub();
     const fail = stub();
     
@@ -131,6 +200,34 @@ test('supertape: operator: stub: calledWithNoArgs', (t) => {
     fn();
     
     t.calledWithNoArgs(fn);
+    t.end();
+});
+
+test('supertape: operator: stub: calledWithNoArgs: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const calledWithNoArgs = operator.calledWithNoArgs({
+        fail,
+    });
+    
+    calledWithNoArgs(fn);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
+    t.end();
+});
+
+test('supertape: operator: stub: calledWithNew: not stub', (t) => {
+    const fn = () => {};
+    const fail = stub();
+    
+    const calledWithNew = operator.calledWithNew({
+        fail,
+    });
+    
+    calledWithNew(fn);
+    
+    t.calledWith(fail, [`Expected stub, but received ${fn.toString()}`]);
     t.end();
 });
 
