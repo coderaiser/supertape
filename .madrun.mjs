c7fdf9964e95a5422207d8e6da1809d9a21bd93d
coderaiser <coderaiser@cloudcmd.io>
2020-12-24 13:21:39 +0200

    chore(supertape) mv out c8 options from madrun to .nycrc.json

--- .madrun.mjs
+++ .madrun.mjs
@@ -4,19 +4,14 @@ const dirs = [
     'packages',
 ];
 
-const C8_OPTIONS = [
-    '--exclude="**/lib/**/{fixture,*.spec.js}"',
-    '--check-coverage --lines 100 --functions 100 --branches 100',
-].join(' ');
-
 export default {
     'test': () => `tape '${dirs}/*/test/*.js' '${dirs}/*/lib/**/*.spec.js' -f progress-bar`,
     'test:tap': () => `tape '${dirs}/*/test/*.js' '${dirs}/*/lib/**/*.spec.js'`,
     'test:fail': async () => `${await run('test')} -f fail`,
     'test:slow': () => 'lerna run test',
     'coverage:long': async () => `c8 ${await run('test')}`,
-    'coverage': async () => `c8 ${C8_OPTIONS} ${await run('test')}`,
-    'coverage:tap': async () => `c8 ${C8_OPTIONS} ${await run('test:tap')}`,
+    'coverage': async () => `c8 ${await run('test')}`,
+    'coverage:tap': async () => `c8 ${await run('test:tap')}`,
     'coverage:slow': () => 'lerna run coverage',
     'lint:slow': () => 'lerna run --no-bail lint',
     'lint-all': async () => `MADRUN_NAME=1 ${await run('lint:*')}`,
--- packages/supertape/.madrun.mjs
+++ packages/supertape/.madrun.mjs
@@ -1,16 +1,11 @@
 import {run} from 'madrun';
 
-const C8_OPTIONS = [
-    '--exclude="**/{bin,lib}/**/{fixture,*.spec.{js,mjs}}"',
-    '--check-coverage --lines 100 --functions 100 --branches 100',
-].join(' ');
-
 export default {
     'test': () => `bin/supertape.js '{bin,lib}/**/*.spec.{js,mjs}'`,
     'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
     'lint': () => 'putout .',
     'fix:lint': () => run('lint', '--fix'),
-    'coverage': () => `c8 ${C8_OPTIONS} npm test`,
+    'coverage': () => `c8 npm test`,
     'report': () => 'c8 report --reporter=text-lcov | coveralls',
     'wisdom': () => run(['lint', 'coverage']),
 };
