49de989fdff1c2dc146f314dd3afa1c903b96efc
coderaiser <coderaiser@cloudcmd.io>
2021-01-09 17:58:42 +0200

    chore(supertape) lint

--- packages/formatter-progress-bar/.madrun.mjs
+++ packages/formatter-progress-bar/.madrun.mjs
@@ -4,6 +4,8 @@ export default {
     'test': () => `supertape 'lib/**/*.spec.js'`,
     'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
     'lint': () => 'putout .',
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
     'coverage': () => `c8 npm test`,
     'report': () => 'c8 report --reporter=text-lcov | coveralls',
