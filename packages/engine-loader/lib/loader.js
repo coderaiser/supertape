6aa9203cf2604782ce9a07234c3dabdc3ee53ccb
coderaiser <coderaiser@cloudcmd.io>
2021-01-23 14:49:42 +0200

    feature(@supertape/engine-loader) improve windows support

--- packages/engine-loader/lib/loader.js
+++ packages/engine-loader/lib/loader.js
@@ -1,13 +1,15 @@
 import {getPaths} from './get-paths.js';
+import {createSimport} from 'simport';
 
 const {assign} = Object;
+const simport = createSimport(import.meta.url);
 
 export const loadOperators = async (operators) => {
     const promises = [];
     const paths = getPaths(operators);
     
     for (const path of paths) {
-        promises.push(import(path));
+        promises.push(simport(path));
     }
     
     const loadedOperators = await Promise.all(promises);
