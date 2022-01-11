d3cbef07d02af26b5b81c24443fe22c29dbc93f5
coderaiser <coderaiser@cloudcmd.io>
2022-01-11 14:57:27 +0200

    chore(supertape) lint

--- packages/supertape/bin/supertape.spec.mjs
+++ packages/supertape/bin/supertape.spec.mjs
@@ -1,17 +1,11 @@
-import {join} from 'path';
-
 import runsome from 'runsome';
-import {
-    createCommons,
-    createSimport,
-} from 'simport';
+import {createSimport} from 'simport';
 
 import test from '../lib/supertape.js';
 
-const {__dirname} = createCommons(import.meta.url);
 const simport = createSimport(import.meta.url);
 
-const name = join(__dirname, 'supertape.mjs');
+const name = new URL('supertape.mjs', import.meta.url).pathname;
 const run = runsome(name);
 
 test('supertape: bin: -v', async (t) => {
