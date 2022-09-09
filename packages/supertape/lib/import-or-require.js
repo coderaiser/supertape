b29d413519fc2572a7e333241fd69fc6b76033e1
Tommy <tmitchell7@uh.edu>
2022-09-09 11:20:25 -0500

    Fix `js` extension detection

--- packages/supertape/lib/import-or-require.js
+++ packages/supertape/lib/import-or-require.js
@@ -6,7 +6,7 @@ const {pathToFileURL} = require('url');
 module.exports.importOrRequire = (cwd, file) => {
     const resolved = resolvePath(cwd, file);
     
-    if (extnamePath(file).endsWith('js')) {
+    if (extnamePath(file).includes('js')) {
         // always resolve before import for windows
         import(pathToFileURL(resolved));
     } else {
