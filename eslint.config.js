51beb8014d68a1e7632493cbc232de4ed1527ebf
coderiaser <coderaiser@cloudcmd.io>
2026-01-30 20:47:26 +0200

    feature: supertape: eslint-plugin-putout v30.0.1

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
 import {safeAlign} from 'eslint-plugin-putout';
-import {mergeESLintConfigs} from '@putout/eslint-flat';
-import {defineConfig} from 'eslint/config';
 
-const config = await mergeESLintConfigs(import.meta.url, 'packages');
-
-export default defineConfig([safeAlign, config]);
+export default safeAlign;
