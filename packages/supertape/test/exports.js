d5c3853761a710c6c303c9a18f9d1014fd4ef441
coderiaser <coderaiser@cloudcmd.io>
2026-03-09 16:29:22 +0200

    chore: lint

--- packages/supertape/test/exports.js
+++ packages/supertape/test/exports.js
@@ -1,19 +1,22 @@
-import {test} from 'supertape';
+import {test, isOnlyTests} from 'supertape';
 import info from '../package.json' with {
-    type: 'json'
-}
+    type: 'json',
+};
 
-import {isOnlyTests} from 'supertape';
+const isFn = (a) => typeof a === 'function';
 
-test('supertape: exports: bin', async (t) => {
+test('supertape: exports: bin', (t) => {
     const {exports} = info;
     const result = exports['./bin/supertape'];
-    const expected =  './bin/supertape.js';
-    t.equal(result, expected)
+    const expected = './bin/supertape.js';
+    
+    t.equal(result, expected);
     t.end();
-})
+});
 
-test('supertape: exports: isOnlyTests', async (t) => {
-    t.ok(isOnlyTests())
+test('supertape: exports: isOnlyTests', (t) => {
+    const result = isFn(isOnlyTests);
+    
+    t.ok(result);
     t.end();
-})
+});
